d2ccc45b96a0eadb97b14b06fed493da0b881055
李重辰 <themaner@qq.com>
2020-03-28 10:12:05 +0800

    同步代码

--- spring-boot/src/main/java/hello/HelloWordMainApplication.java
+++ spring-boot/src/main/java/hello/HelloWordMainApplication.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * SpringBootApplication 标注一个主程序类，说明这是一个SprintBoot应用
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 李重辰
  * @date 2020/3/24 0:08
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class HelloWordMainApplication {
   public static void main(String[] args) {
     // Spring应用程序启动起来
