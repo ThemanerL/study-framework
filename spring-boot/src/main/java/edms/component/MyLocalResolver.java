d90271c2db674a3bf966cb37e5fafa78c76938a9
李重辰 <themaner@qq.com>
2020-07-06 19:17:30 +0800

    新增国际化配置

---
+++ spring-boot/src/main/java/edms/component/MyLocalResolver.java
@@ -0,0 +1,31 @@
+package edms.component;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * 可以在链接上携带区域信息
+ *
+ * @author 李重辰
+ * @date 2020/7/6 18:30
+ */
+public class MyLocalResolver implements LocaleResolver {
+  @Override
+  public Locale resolveLocale(HttpServletRequest request) {
+    String language = request.getParameter("l");
+    if (!StringUtils.isEmpty(language)) {
+      String[] split = language.split("_");
+      return new Locale(split[0], split[1]);
+    }
+    return Locale.getDefault();
+  }
+
+  @Override
+  public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+  }
+}
--- spring-boot/src/main/java/edms/config/MvcConfig.java
+++ spring-boot/src/main/java/edms/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package edms.config;
 
 
+import edms.component.MyLocalResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +20,8 @@ public class MvcConfig implements WebMvcConfigurer {
     registry.addViewController("/viewController").setViewName("success");
   }
 
+  @Bean
+  public LocaleResolver localeResolver() {
+    return new MyLocalResolver();
+  }
 }
