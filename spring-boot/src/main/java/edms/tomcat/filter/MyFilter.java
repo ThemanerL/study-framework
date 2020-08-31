faa167b387bd219de1f44542480dd59de42739c1
李重辰 <themaner@qq.com>
2020-08-31 23:30:07 +0800

    修复拦截器拦截了所有请求的问题

--- spring-boot/src/main/java/edms/tomcat/filter/MyFilter.java
+++ spring-boot/src/main/java/edms/tomcat/filter/MyFilter.java
@@ -14,6 +14,6 @@ import java.io.IOException;
 public class MyFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+    chain.doFilter(request, response);
   }
 }
